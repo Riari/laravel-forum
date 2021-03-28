6a50742e0ee405e470ba933ddd2ad823017c20ff
Riari <3583774+Riari@users.noreply.github.com>
2021-03-28 10:55:12 +0100

    Ensure category content is removed upon deletion

--- src/Actions/DeleteCategory.php
+++ src/Actions/DeleteCategory.php
@@ -2,7 +2,10 @@
 
 namespace TeamTeaTime\Forum\Actions;
 
+use Illuminate\Support\Facades\DB;
 use TeamTeaTime\Forum\Models\Category;
+use TeamTeaTime\Forum\Models\Post;
+use TeamTeaTime\Forum\Models\Thread;
 
 class DeleteCategory extends BaseAction
 {
@@ -15,6 +18,21 @@ class DeleteCategory extends BaseAction
 
     protected function transact()
     {
+        $threadIdsToDelete = [];
+        if (! $this->category->isEmpty())
+        {
+            $descendantIds = $this->category->descendants->pluck('id')->toArray();
+            $threadIdsToDelete += Thread::whereIn('category_id', $descendantIds)->withTrashed()->pluck('id')->toArray();
+
+            Category::whereIn('id', $descendantIds)->delete();
+        }
+
+        $threadIdsToDelete = array_merge($threadIdsToDelete, $this->category->threads()->withTrashed()->pluck('id')->toArray());
+
+        Post::whereIn('thread_id', $threadIdsToDelete)->withTrashed()->forceDelete();
+        Thread::whereIn('id', $threadIdsToDelete)->withTrashed()->forceDelete();
+        DB::table(Thread::READERS_TABLE)->whereIn('thread_id', $threadIdsToDelete)->delete();
+
         return $this->category->delete();
     }
 }
